418b194750ee553b72aa36c5e1efaa9ee5e9a445
silencespeaks7 <63138395+silencespeaks7@users.noreply.github.com>
2020-06-24 22:13:35 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,24 +18,24 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 Hi {}, my name is {}! 
-I am an Anime themed group management bot with some fun extras [;)](https://telegra.ph/file/095d7e696096e21b06447.jpg)
+I am an Anime themed group management bot with some fun extras [;)](https://telegra.ph/file/ab9a089aaec37716c913f.jpg)
 
 You can find the list of available commands with /help.
 
 [Kigyō's Repo](https://github.com/Dank-del/EnterpriseALRobot) 
 
-The support group chat is at @YorktownEagleUnion
+The support group chat is at @Sararprobot
 """
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a part of [Eagle Union](https://telegra.ph/file/095d7e696096e21b06447.jpg)
+I'm a part of [Sararp](https://telegra.ph/file/cdec177155100460cc8c7.jpg)
 
 Have a look at the following for an idea of some of \
 the things I can help you with.
 
-AI and blacklist module from [TheRealPhoenixBot](https://github.com/rsktg/TheRealPhoenixBot.git)
-Source code from [SaitamaRobot](https://github.com/AnimeKaizoku/SaitamaRobot)
+
+
 
 *Main* commands available:
  - /start: start the bot
@@ -52,10 +52,10 @@ And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
 
-KIGYO_IMG = "https://telegra.ph/file/e5100e06c03767af80023.jpg"
+KIGYO_IMG = "https://telegra.ph/file/cdec177155100460cc8c7.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-Kigyō is hosted on one of Kaizoku's Servers and doesn't require any donations as of now but \
+Sara is hosted on one of Kaizoku's Servers and doesn't require any donations as of now but \
 You can donate to the original writer of the Base code, Paul
 There are two ways of supporting him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
 
@@ -147,7 +147,7 @@ def start(bot: Bot, update: Update, args: List[str]):
             first_name = update.effective_user.first_name
             update.effective_message.reply_photo(KIGYO_IMG,
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Add Kigyō to your group",
+                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Add Sara to your group",
                                                                        url="t.me/{}?startgroup=true".format(bot.username))]]))
     else:
         update.effective_message.reply_text("Yo, whadup?")
