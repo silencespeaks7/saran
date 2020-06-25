91a9d93758505df7311a0b960d707a43bfb454f4
silencespeaks7 <63138395+silencespeaks7@users.noreply.github.com>
2020-06-25 17:41:17 +0530

    Update natalie_bot_modules_sql_locks_sql.py

--- tg_bot/modules/sql/natalie_bot_modules_sql_locks_sql.py
+++ tg_bot/modules/sql/natalie_bot_modules_sql_locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from natalie_bot.modules.sql import SESSION, BASE
+from tg_bot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
