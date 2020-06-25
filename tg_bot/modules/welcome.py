0b9bef2d5669de8adf1a9b6afc2bc704d3481eda
silencespeaks7 <63138395+silencespeaks7@users.noreply.github.com>
2020-06-25 08:52:18 +0530

    Update welcome.py

--- tg_bot/modules/welcome.py
+++ tg_bot/modules/welcome.py
@@ -732,17 +732,17 @@ __help__ = """
 {}
 
 *Admins only:*
- • `/welcome <on/off>`*:* enable/disable welcome messages.
- • `/welcome`*:* shows current welcome settings.
- • `/welcome noformat`*:* shows current welcome settings, without the formatting - useful to recycle your welcome messages!
- • `/goodbye`*:* same usage and args as `/welcome`.
+ • `/welcome <on/off>:enable/disable welcome messages.
+ • `/welcome:shows current welcome settings.
+ • `/welcome noformat:shows current welcome settings, without the formatting - useful to recycle your welcome messages!
+ • `/goodbye:same usage and args as `/welcome`.
  • `/setwelcome <sometext>`*:* set a custom welcome message. If used replying to media, uses that media.
  • `/setgoodbye <sometext>`*:* set a custom goodbye message. If used replying to media, uses that media.
- • `/resetwelcome`*:* reset to the default welcome message.
- • `/resetgoodbye`*:* reset to the default goodbye message.
- • `/cleanwelcome <on/off>`*:* On new member, try to delete the previous welcome message to avoid spamming the chat.
- • `/welcomemutehelp`*:* gives information about welcome mutes.
- • `/welcomehelp`*:* view more formatting information for custom welcome/goodbye messages.
+ • `/resetwelcome:reset to the default welcome message.
+ • `/resetgoodbye:reset to the default goodbye message.
+ • `/cleanwelcome <on/off>:On new member, try to delete the previous welcome message to avoid spamming the chat.
+ • `/welcomemutehelp:gives information about welcome mutes.
+ • `/welcomehelp:view more formatting information for custom welcome/goodbye messages.
  • `/cleanservice <on/off`*:* deletes telegrams welcome/left service messages. 
  *Example:* user joined chat, user left chat.
 """.format(WELC_HELP_TXT)
