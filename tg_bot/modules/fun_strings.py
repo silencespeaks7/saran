47e0fb9f6f47f99f0172668e20017b957cb48811
silencespeaks7 <63138395+silencespeaks7@users.noreply.github.com>
2020-06-26 11:12:34 +0530

    Update fun_strings.py

--- tg_bot/modules/fun_strings.py
+++ tg_bot/modules/fun_strings.py
@@ -836,3 +836,5 @@ TABLE = (
     "I ran out of tables, will order more.",
     "Go do some work instead of flippin tables."
 )
+GOODMORNING_STRING = (
+          "Welcome this beautiful morning with a smile on your face. I hope you’ll have a great day today. Wishing you a very good morning!."
